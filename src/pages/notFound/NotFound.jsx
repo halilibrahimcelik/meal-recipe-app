8edd1355d814f133be6749c597cc29f7d2aeca2e
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-12 18:25:18 +0300

    fixing a bug

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <>
     
-    <h1 style={{textAlign:"center", color:"white",margin:"3rem"}}>NOT FOUND :(</h1>
+    <h1 style={{textAlign:"center", color:"#E93E44",margin:"3rem"}}>NOT FOUND :(</h1>
     
     </>
   )
